d56f08a4a72f4ea41b1c5091a2872589848a276b
Adam Fortier <fort7350@mylaurier.ca>
2021-04-02 07:11:31 -0700

    Initial Commit

---
+++ fort7350_project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  fort7350_project
+//
+//  Created by Adam Fortier on 2021-04-02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
